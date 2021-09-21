e8e065b2b4ba2e515b34fd7a500382599590ab1e
jwdagney55 <jwdagney@udel.edu>
2021-09-21 00:01:46 -0400

    Cards have images, and functionality

--- src/Game-Board/Game-Board.tsx
+++ src/Game-Board/Game-Board.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import ReactCardFlip from 'react-card-flip';
 import Card from '../components/Card';
+import logo from '../images/baseball.png'
+import basket from '../images/basketball.png'
+
 
 
 import './Game-Board.css';
 
 function GameBoard() {
     //Set state of card to be not flipped
+    //Hard code the cards in for now
+    let sportCards: string[] = ['../images/baseball.png', '../images/basketball.png', '../images/tennis.png'];
+
 
     return(
         <div className="Game-Board">
             <h2>Match Game!</h2>
-            <table>
-                <tr>
-                    <td><Card cardNum={"1"} word={"apple"} /></td>
-                    <td><Card cardNum={"2"} word={"apple"} /></td>
-                    <td><Card cardNum={"3"} word={"orange"} /></td>
+            
+            <table> 
+                 <tr> 
+                    <td><Card cardNum={1} word={"tennis"} /></td>
+                    <td><Card cardNum={2} word={"tennis"} /></td>
+                    <td><Card cardNum={3} word={"basketball"} /></td>
                 </tr>
                 <tr>
-                    <td><Card cardNum={"4"} word={"banana"} /></td>
-                    <td><Card cardNum={"5"} word={"banana"} /></td>
-                    <td><Card cardNum={"6"} word={"orange"} /></td>
-                </tr>
+                    <td><Card cardNum={4} word={"baseball"} /></td>
+                    <td><Card cardNum={5} word={"baseball"} /></td>
+                    <td><Card cardNum={6} word={"basketball"} /></td>
+                     </tr>
             </table>
         </div>
     )
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,23 +1,86 @@
 import React from 'react';
 import ReactCardFlip from 'react-card-flip';
-
+import baseball from '../images/baseball.png'
+import basketball from '../images/basketball.png'
+import tennis from '../images/Tennis.png'
+import card1 from '../images/Card1.png'
+import card2 from '../images/Card2.png'
+import card3 from '../images/Card3.png'
+import card4 from '../images/Card4.png'
+import card5 from '../images/Card5.png'
+import card6 from '../images/Card6.png'
+import notFound from '../images/notfound.png'
 
 import '../Game-Board/Game-Board.css';
 
-function Card(stuff: {cardNum: string; word: string}): JSX.Element {
+function Card(stuff: {cardNum: number; word: string}): JSX.Element {
+    console.log("In Card");
     const [isFlipped, setIsFlipped] = React.useState(false);
 
     const handleClick = () => {
         setIsFlipped(!isFlipped);
     }
 
-    return (
-        <ReactCardFlip isFlipped = {isFlipped} flipDirection="vertical">
-            <td onClick={handleClick}>{stuff.cardNum}</td>
+    let cardPics: string[] = ["baseball", "basketball", "tennis"];
+    let cardNums: number[] = [1,2,3,4,5,6];
 
-            <td onClick={handleClick}>{stuff.word}</td>
+    const FindNum = (num:number): JSX.Element => {
+        switch(num){
+            case (1):
+                return <img src={card1} onClick={handleClick} alt="Card1"/>;
+            case (2):
+                return <img src={card2} onClick={handleClick} alt="Card2"/>;
+            case (3):
+                return <img src={card3} onClick={handleClick} alt="Card3"/>;
+            case (4):
+                return <img src={card4} onClick={handleClick} alt="Card4"/>;
+            case (5):
+                return <img src={card5} onClick={handleClick} alt="Card5"/>;
+            case (6):
+                return <img src={card6} onClick={handleClick} alt="Card6"/>;
+        }
+        return <img src={notFound} onClick={handleClick} alt="image not found"/>;
+    }
+
+    const FindCard = (sport:string): JSX.Element => {
+        switch(sport){
+            case ("tennis"):
+                console.log("tennis card")
+                return <img src={tennis} onClick={handleClick} alt="tennis"/>;
+            case ("baseball"):
+                console.log("baseball card")
+                return <img src={baseball} onClick={handleClick} alt="baseball"/>;
+            case ("basketball"):
+                console.log("basketball card")
+                return <img src={basketball} onClick={handleClick} alt="basketball"/>;
+        }
+        console.log("no card")
+        return <img src={notFound} onClick={handleClick} alt="image not found"/>;
+    }
+
+    return (
+        
+        <ReactCardFlip isFlipped = {isFlipped} flipDirection="vertical">
+            {FindNum(stuff.cardNum)}
+
+            {FindCard(stuff.word)}
         </ReactCardFlip>
+
+
     )
+
+    {/**
+    return (
+        <table>
+            <tr>
+                <td>cardNumElement</td>
+                <td>cardSportElement</td>
+            </tr>
+        </table>
+        
+    );
+    */}
 }
+
 
 export default Card;
