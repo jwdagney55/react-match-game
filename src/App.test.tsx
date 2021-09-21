9fb045da671feeda216407ea239f82491b2cd894
jwdagney55 <jwdagney@udel.edu>
2021-09-21 12:13:30 -0400

    Trying to write some tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import GameBoard from './Game-Board/Game-Board'
+import Card from './components/Card'
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('GameBoard', () => {
+  render(<GameBoard />)
+  it("has the Game Board when the application loads",()=> {
+    const element = screen.getByText("Match Game!");
+
+    expect(element).toBeInTheDocument;
+  });
+})
