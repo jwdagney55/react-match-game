17e2f351583c3df00eb3c94ec1292c4ac9ccc02f
jwdagney55 <jwdagney@udel.edu>
2021-09-23 23:01:59 -0400

    A lot better card functions

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,13 +7,6 @@ import basketball from '/images/basketball.png'
 import tennis from '/images/tennis.png'
 */
 
-import card1 from '../images/Card1.png'
-import card2 from '../images/Card2.png'
-import card3 from '../images/Card3.png'
-import card4 from '../images/Card4.png'
-import card5 from '../images/Card5.png'
-import card6 from '../images/Card6.png'
-import notFound from '../images/notfound.png'
 
 import '../Game-Board.css';
 
@@ -25,48 +18,21 @@ function Card(stuff: {cardNum: number; word: string}): JSX.Element {
     const handleClick = () => {
         setIsFlipped(!isFlipped);
     }
-    /*
-    const indexImgImport = (): void => {
-        for(let x in SPORTS){
-            console.log(typeof(x), "is ", x);
-            console.log(1);
-        }
 
-    }
-    */
 
     const FindNum = (num:number): JSX.Element => {
-        switch(num){
-            case (1):
-                return <img className = "Game-Card" src={card1} onClick={handleClick} alt="Card1"/>;
-            case (2):
-                return <img className = "Game-Card" src={card2} onClick={handleClick} alt="Card2"/>;
-            case (3):
-                return <img className = "Game-Card" src={card3} onClick={handleClick} alt="Card3"/>;
-            case (4):
-                return <img className = "Game-Card" src={card4} onClick={handleClick} alt="Card4"/>;
-            case (5):
-                return <img className = "Game-Card" src={card5} onClick={handleClick} alt="Card5"/>;
-            case (6):
-                return <img className = "Game-Card" src={card6} onClick={handleClick} alt="Card6"/>;
-        }
-        return <img src={notFound} onClick={handleClick} alt="not found"/>;
+        return <img className="Game-Card" src={process.env.PUBLIC_URL + "/images/card" + num + ".png"} onClick={handleClick} alt={num.toString()} />;
     }
 
     const FindCard = (sport:string): JSX.Element => {
-        switch(sport){
-            case ("tennis"):
-                console.log("tennis card")
-                return <img className = "Game-Card" src={process.env.PUBLIC_URL + "/images/tennis.png"} onClick={handleClick} alt="tennis"/>;
-            case ("baseball"):
-                console.log("baseball card")
-                return <img className = "Game-Card" src={process.env.PUBLIC_URL + "/images/baseball.png"} onClick={handleClick} alt="baseball"/>;
-            case ("basketball"):
-                console.log("basketball card")
-                return <img className = "Game-Card" src={process.env.PUBLIC_URL + "/images/basketball.png"} onClick={handleClick} alt="basketball"/>;
+        //Attempting to catch an error if the file does not exist, but don't know how to do that.
+        try {
+            console.log("trying");
+            return <img className="Game-Card" src={process.env.PUBLIC_URL + "/images/" + sport + ".png"} onClick={handleClick} alt={sport} />;
+        } catch (error) {
+            console.log(error, "found error");
+            return <img className="Game-Card" src={process.env.PUBLIC_URL + "/images/notfound.png"} onClick={handleClick} alt={sport} />;
         }
-        console.log("no card")
-        return <img className = "Game-Card" src={notFound} onClick={handleClick} alt="not found"/>;
     }
 
     return (
@@ -74,7 +40,7 @@ function Card(stuff: {cardNum: number; word: string}): JSX.Element {
         <ReactCardFlip isFlipped = {isFlipped} flipDirection="vertical" >
             {FindNum(stuff.cardNum)}
 
-            {FindCard(stuff.word)}
+            {FindCard("hello")}
         </ReactCardFlip>
         
        /*
