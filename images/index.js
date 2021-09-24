5c3b17d185082e289d120834dabd2c5ff7921196
jwdagney55 <jwdagney@udel.edu>
2021-09-23 22:50:09 -0400

    Updates

---
+++ images/index.js
@@ -0,0 +1,11 @@
+export const baseball = require('./baseball.png');
+export const basketball = require('./basketball.png');
+export const tennis = require('./tennis.png');
+export const card1 = require('./Card1.png');
+export const card2 = require('./Card2.png');
+export const card3 = require('./Card3.png');
+export const card4 = require('./Card4.png');
+export const card5 = require('./Card5.png');
+export const card6 = require('./Card6.png');
+export const notFound = require('./notfound.png');
+
